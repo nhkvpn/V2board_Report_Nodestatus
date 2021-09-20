2e7fbe63eef2d3437a6c8672fc6c1ebc8a389863
萌新 <niuziyi233@outlook.com>
2021-09-20 20:45:13 +0800

    Create traffic.php

---
+++ traffic.php
@@ -0,0 +1,16 @@
+<?php
+include "lib/sent.php";
+include "config.php";
+include "lib/login.php";
+exec("curl https://".$hostname."/api/v1/admin/stat/getServerLastRank -b logined.cookie",$return);
+$json=json_decode($return[0],true);
+$text=$name."今天的流量统计情况\n";
+if($show_poweredby){
+    $text.="Powered By MengXin";
+}
+for($i=0;$i<count($json)-1;$i++){
+  $text.="\n".$json["data"][$i]["server_name"]." 今天跑了 ".$json["data"][$i]["total"]."流量"
+}
+include "lib/make.php";
+send($data);
+?>

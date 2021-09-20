0f350f75e3f4b187d8733cb966d4f283cef0a536
萌新 <niuziyi233@outlook.com>
2021-09-20 20:27:54 +0800

    Create make.php

---
+++ lib/make.php
@@ -0,0 +1,8 @@
+<?php
+$data = array(
+        "chat_id" => $chat_id,
+        "text" => $text,
+        "disable_web_page_preview" => true,
+        "reply_to_message_id" => $messageid
+            );
+?>

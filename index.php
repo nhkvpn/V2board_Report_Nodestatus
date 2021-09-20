8b49b33ebd74fe69797852fb0bdd38d925f23cc1
萌新 <niuziyi233@outlook.com>
2021-09-20 20:21:48 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,23 +1,4 @@
 <?php
-function send($data) {
-    global $token;
-    $data_string = json_encode($data);
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_POST, 1);
-    curl_setopt($ch, CURLOPT_URL,"https://api.telegram.org/bot".$token."/sendMessage");
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-        'Content-Type: application/json; charset=utf-8',
-        'Content-Length: ' . strlen($data_string))
-    );
-    ob_start();
-    curl_exec($ch);
-    $return_content = ob_get_contents();
-    ob_end_clean();
-    $return_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-    $return_content = json_decode($return_content,true);
-    return $return_content;
-}
 include "config.php";
 exec("curl https://".$hostname."/api/v1/passport/auth/login -X POST -d 'email=".$admin_username."&password=".$admin_password."' -c logined.cookie",$a);
 exec("curl https://".$hostname."/api/v1/admin/server/manage/getNodes -b logined.cookie",$return);

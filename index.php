5cdd073b1ff955507acc92bca273a9af869b8309
萌新 <niuziyi233@outlook.com>
2021-09-20 20:17:08 +0800

    Update index.php

--- index.php
+++ index.php
@@ -30,13 +30,22 @@ for($i=0;$i<count($json['data'])-1;$i++){
     if($json['data'][$i]['show']!=null){
         if($json['data'][$i]['parent_id']==null){
             if($json['data'][$i]['online']==null){
-                $text.="\n".$json['data'][$i]['name']." 在线人数:0";
+		    if($json['data'][$i]['availale_status']==null){
+                $text.="\n".$json['data'][$i]['name']." Shit,这台机器炸了";
+			    } else {
+		   	 $text.="\n".$json['data'][$i]['name']." 在线人数:0";
+		    }
             } else {
                 $text.="\n".$json['data'][$i]['name']." 在线人数:".$json['data'][$i]['online'];
             }
         } else {
+		
             if($json['data'][$json['data'][$i]['parent_id']]['online']==null){
-                $text.="\n".$json['data'][$i]['name']." 在线人数:0";
+		    if($json['data'][$i]['availale_status']==null){
+                $text.="\n".$json['data'][$i]['name']." Shit,这台机器炸了";
+			    } else {
+		   	 $text.="\n".$json['data'][$i]['name']." 在线人数:0";
+		    }
             } else {
                 $text.="\n".$json['data'][$i]['name']." 在线人数:".strval($json['data'][strval($json['data'][$i]['parent_id'])]['online']);
             }

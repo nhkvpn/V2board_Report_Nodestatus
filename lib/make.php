dc32fdee2f03ca241c64c3af12fd758d401d321b
萌新 <mengxin82@outlook.com>
2021-10-30 14:07:42 +0800

    Delete make.php

--- lib/make.php
+++
@@ -1,8 +0,0 @@
-<?php
-$data = array(
-        "chat_id" => $chat_id,
-        "text" => $text,
-        "disable_web_page_preview" => true,
-        "reply_to_message_id" => $messageid
-            );
-?>
